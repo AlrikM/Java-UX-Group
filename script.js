8ae6fe2dfa22facd443a19baffc8e664b6da6fbd
GurjotKaur1501 <jotkau0115@gmail.com>
2025-04-16 10:39:58 +0200

    adding code

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('addBtn').addEventListener('click', function() {
+    alert('Add button clicked! Implement file upload functionality here.');
+});
+
+document.getElementById('removeBtn').addEventListener('click', function() {
+    alert('Remove button clicked! Implement file removal functionality here.');
+});
+
+document.getElementById('applyBtn').addEventListener('click', function() {
+    alert('Apply changes!');
+});
+
+document.getElementById('cancelBtn').addEventListener('click', function() {
+    alert('Changes cancelled!');
+});
